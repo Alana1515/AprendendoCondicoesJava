00619bf6a992c93f7d6a755379dddad229c11e19
gustavomullersouza <gustavomullerdesouza@gmail.com>
2024-08-22 07:56:46 -0300

    Criação do projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer IdadePessoa = 20;
+        String NomePassoa = "Alana";
+
+        /*
+         * Operadores Logicos
+         * >= Maior igual
+         * < Manor
+         * <= Menor igual
+         * == é igual
+         * != é diferente
+         * equal compara Textos
+         * */
+
+
+        if (IdadePessoa > 21) {
+            System.out.println("é maior que 21");
+        } else {
+            System.out.println("Não é maior");
+        }
+
     }
+
+
 }
